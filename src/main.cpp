e48a280313b2f56dbfc58339cab2bf1576d0201b
DESKTOP-C4LU9U7\Vitech-UA <viktorstarovit@gmail.com>
2022-02-05 23:47:43 +0200

    Додав два сенсора BME280 для вимірюваннь на вул і в кімнаті.

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,14 @@
 
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
 
 // Replace with your network credentials
 const char* ssid     = "Nata";
 const char* password = "L12q38w2";
+#define SEALEVELPRESSURE_HPA (1013.25) 
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -21,8 +25,10 @@ String output5State = "off";
 String output4State = "off";
 
 // Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
+const int output5 = 15;
+const int output4 = 0;
+Adafruit_BME280 OutDorSensor;
+Adafruit_BME280 InDorSensor;
 
 void setup() {
   Serial.begin(115200);
@@ -47,30 +53,39 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   server.begin();
+  
+  if(!OutDorSensor.begin(0x77)){
+    Serial.println("Could not find a outdor BME280 sensor, check wiring!"); 
+    while (1);                                          
+  
+  }
+  if(!InDorSensor.begin(0x76)){
+    Serial.println("Could not find a indor BME280 sensor, check wiring!"); 
+    while (1);                                          
+  
+  }
+  
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+  WiFiClient client = server.available();               // Получаем данные, посылаемые клиентом 
+ 
+  if (client) {                                         
+    Serial.println("New Client.");                      // Отправка "Новый клиент"
+    String currentLine = "";                            // Создаем строку для хранения входящих данных от клиента
+    while (client.connected()) {                        // Пока есть соединение с клиентом 
+      if (client.available()) {                         // Если клиент активен 
+        char c = client.read();                         // Считываем посылаемую информацию в переменную "с"
+        Serial.write(c);                                // Отправка в Serial port 
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {                                
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
+            client.println("HTTP/1.1 200 OK");          
+            client.println("Content-type:text/html ");
+            client.println("Connection: close");        
+            client.println("Refresh: 10");              
             client.println();
-            
+            //Кнопки LED-------------------------------------------------------------
             // turns the GPIOs on and off
             if (header.indexOf("GET /5/on") >= 0) {
               Serial.println("GPIO 5 on");
@@ -90,8 +105,7 @@ void loop(){
               digitalWrite(output4, LOW);
             }
             
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
+
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
             // CSS to style the on/off buttons 
@@ -121,23 +135,73 @@ void loop(){
             } else {
               client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
+            ///-----------------------------------------------------------------------
+            //Зовнішній сенсор
+            client.println("<!DOCTYPE html><html>");    
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<meta charset='UTF-8'>");   
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
+            client.println("table { border-collapse: collapse; width:40%; margin-left:auto; margin-right:auto; }");
+            client.println("th { padding: 12px; background-color: #0043af; color: white; }");
+            client.println("tr { border: 1px solid #ddd; padding: 12px; }");
+            client.println("tr:hover { background-color: #bcbcbc; }");
+            client.println("td { border: none; padding: 12px; }");
+            client.println(".sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px; }");
+            client.println("</style></head><body><h1></h1>");
+            client.println("<table><tr><th>Параметр(На вулицi)</th><th>Показники</th></tr>");
+            client.println("<tr><td>Температура</td><td><span class=\"sensor\">");
+            client.println(OutDorSensor.readTemperature());
+            client.println(" *C</span></td></tr>");    
+            client.println("<tr><td>Тиск</td><td><span class=\"sensor\">");
+            client.println((OutDorSensor.readPressure() / 100.0F) * 0.75006375541921);
+            client.println(" mmHg</span></td></tr>");
+            client.println("<tr><td>Приблизна висота над рiвнем моря</td><td><span class=\"sensor\">");
+            client.println(OutDorSensor.readAltitude(SEALEVELPRESSURE_HPA));
+            client.println(" m</span></td></tr>"); 
+            client.println("<tr><td>Вологiсть</td><td><span class=\"sensor\">");
+            client.println(OutDorSensor.readHumidity());
+            client.println(" %</span></td></tr>"); 
             client.println("</body></html>");
             
-            // The HTTP response ends with another blank line
+            //Внутрішній сенсор сенсор
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<meta charset='UTF-8'>");   
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
+            client.println("table { border-collapse: collapse; width:40%; margin-left:auto; margin-right:auto; }");
+            client.println("th { padding: 12px; background-color: #0043af; color: white; }");
+            client.println("tr { border: 1px solid #ddd; padding: 12px; }");
+            client.println("tr:hover { background-color: #bcbcbc; }");
+            client.println("td { border: none; padding: 12px; }");
+            client.println(".sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px; }");
+            client.println("</style></head><body><h1></h1>");
+            client.println("<table><tr><th>Параметр(В хаті)</th><th>Показники</th></tr>");
+            client.println("<tr><td>Температура</td><td><span class=\"sensor\">");
+            client.println(InDorSensor.readTemperature());
+            client.println(" *C</span></td></tr>");    
+            client.println("<tr><td>Тиск</td><td><span class=\"sensor\">");
+            client.println((InDorSensor.readPressure() / 100.0F) * 0.75006375541921);
+            client.println(" mmHg</span></td></tr>");
+            client.println("<tr><td>Приблизна висота над рiвнем моря</td><td><span class=\"sensor\">");
+            client.println(InDorSensor.readAltitude(SEALEVELPRESSURE_HPA));
+            client.println(" m</span></td></tr>"); 
+            client.println("<tr><td>Вологiсть</td><td><span class=\"sensor\">");
+            client.println(InDorSensor.readHumidity());
+            client.println(" %</span></td></tr>"); 
+            
+            client.println("</body></html>");
             client.println();
-            // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else { 
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {  
+          currentLine += c;      
         }
       }
     }
-    // Clear the header variable
     header = "";
-    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
